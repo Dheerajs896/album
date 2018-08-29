9651903c167c46aa5ec1997767432683716084d0
Dheeraj Singh <dheerajs896@gmail.com>
2018-08-30 00:43:08 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -1,16 +1,15 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import Header from './src/components/Header';
+import AlbumList from './src/components/AlbumList';
+
 
 export default class App extends React.Component {
   render() {
     return (
       <View>
-        <Header />
-        <Header />
-        <Header />
-        <Header />
-        <Header />
+        <Header getHeader= {'Albem'} />
+        <AlbumList />
       </View>
     );
   }
